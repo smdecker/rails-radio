c41317b3e3159771fc53b559166cca07d088ddad
smdecker <sdeckerm@gmail.com>
2018-06-25 22:31:14 -0400

    Add Google OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  has_and_belongs_to_many :oauth_credentials
   
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
   attr_writer :login
 
   def login
@@ -20,5 +23,15 @@ class User < ApplicationRecord
     end
   end
 
-
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    @user = User.where(email: data['email']).first
+    unless @user
+      @user = User.create(username: data['name'],
+         email: data['email'],
+         password: Devise.friendly_token[0,20]
+      )
+    end    
+    	@user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:show]
 
-  devise_for :users, path: 'user'
+  devise_for :users, path: 'user', controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'home#index'
 end
