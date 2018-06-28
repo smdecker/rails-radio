7481dab5afd31d4178ccaff78b936642690f0f84
smdecker <sdeckerm@gmail.com>
2018-06-28 17:35:27 -0400

    Add most popular episodes scope

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 	def index
 		@episodes = Episode.all
+		@admin = User.where(admin: true)
 	end
 end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -9,4 +9,20 @@ class Episode < ApplicationRecord
 
 	extend FriendlyId
   friendly_id :title, use: :slugged
+
+	def most_popular
+		self.favorited_by.each_with_index {|u, i| @total = i +=1} 
+		@total.to_i
+	end
+
+	def self.sorted_by_most_popular
+		Episode.all.sort_by {|e| e.most_popular}.reverse
+	end
 end
+
+# Most popular by week
+	# def self.sorted_by_most_popular
+	# 	@scoped = Episode.all.where(created_at: 1.week.ago..Time.now)
+	# 	@scoped.sort_by {|e| e.most_popular}.reverse
+	# end
+
