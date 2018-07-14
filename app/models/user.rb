3f2ceb7c10ced45fca0a85ef87d4a8a11ee76703
smdecker <sdeckerm@gmail.com>
2018-07-14 00:33:59 -0400

    Comment section update

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :favorite_episodes
   has_many :favorites, through: :favorite_episodes, source: :episode
 
-  has_attached_file :avatar, styles: { thumb: "70x70#" }, default_url: "/assets/:style/missing.png"
+  has_attached_file :avatar, styles: { thumb: "70x70#" }, default_url: "/assets/:style/missing_profile.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   has_and_belongs_to_many :oauth_credentials
